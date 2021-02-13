24913f47d7068632436b76039cd2abfa29c863bf
Kyle Mayer <kmayer1@cnm.edu>
2021-02-13 11:26:11 -0700

    fixed problems with mailgun and backend

--- src/index.js
+++ src/index.js
@@ -7,15 +7,19 @@ const Recaptcha = require("express-recaptcha").RecaptchaV2;
 const mailgun = require("mailgun-js");
 const {check, validationResult} = require("express-validator");
 
+//array holding check functions for validation of fields
 const validation = [
     check("name", "A valid name is required.").not().isEmpty().trim().escape(),
-    check("email", "Please provide a valid email address.").isEmail(),
-    check("purpose").optional().trim().escape(),
-    check("message", "A message shorter than 2,000 characters is required.").trim().escape().isLength({min: 1, max:2000})
+    check("email", "Please provide a valid email").isEmail(),
+    check("subject").optional().trim().escape(),
+    check("message", "A message shorter than 2000 characters is required").trim().escape().isLength({min:1, max:2000})
 ];
 
+//set up express
 const app = express();
+//set up recaptcha object
 const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY);
+//set up mailgun object
 const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
 
 
@@ -34,29 +38,31 @@ const handleGetRequest = (req, res) => {
 const handlePostRequest = (req, res, nextFunction) => {
     res.append("Content-Type", "text/html")
     console.log(req.body);
-    // if(request.recaptcha.error) {
-    //     return send(`<div class="alert alert-danger" role='alert'><strong>There was an error with recaptcha!</strong></div>`)
-    // }
+    if(req.recaptcha.error) {
+        return res.send(`<div class="alert alert-danger" role='alert'><strong>There was an error with recaptcha!</strong></div>`)
+    }
 
     const errors = validationResult(req)
 
     if(errors.isEmpty() === false) {
         const currentError = errors.array()[0]
-        return res.send(`<div class="alert alert.danger" role'alert'><strong>$currentError.msg}</strong></div>`)
+        return res.send(`<div class="alert alert-danger" role="alert"><strong>${currentError.msg}</strong></div>`)
     }
 
-    const {email, name, purpose, message} = req.body;
+    //object destructuring to pull out content from request body object
+    const {email, name, subject, message} = req.body;
     const mailgunData = {
         to: process.env.MAIL_RECIPIENT,
-        from: `${name} <postmaster@${process.env.MAILGUN_API_KEY}>`,
-        subject: `${email}: ${purpose}`,
+        from: `${name} <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+        subject: `${email}: ${subject}`,
         text: `${message}`
     }
 
-    mg.messages().send(mailgunData, () => {
+    mg.messages().send(mailgunData, (error) => {
         if(error) {
-            return response.send(Buffer.from(`<div class='alert alert-danger' role='alert'>Unable to send email!</div>`))
+            return res.send(Buffer.from(`<div class='alert alert-danger' role='alert'>Unable to send email!</div>`))
         }
+        return res.send(Buffer.from("<div class='alert alert-success' role='alert'>Email successfully sent.</div>"))
     });
 }
 
